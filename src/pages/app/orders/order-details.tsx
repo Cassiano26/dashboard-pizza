041e34fc405ecf40d6b6c22c8a689b7bdef83141
Cassiano26 <candidocassiano2@gmail.com>
2024-10-24 14:23:54 -0300

    style: Order details skeleton.

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -20,6 +20,7 @@ import {
 } from '@/components/ui/table'
 
 import { getOrderDetails } from './../../../api/get-order-details'
+import { OrderDetailsSkeleton } from './order-details-skeleton'
 
 export interface OrderDetailsProps {
   orderId: string
@@ -42,7 +43,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
         <DialogTitle>pedido: {orderId}</DialogTitle>
         <DialogDescription>Detalhes do pedido</DialogDescription>
       </DialogHeader>
-      {order && (
+      {order ? (
         <div className="space-y-6">
           <Table>
             <TableBody>
@@ -139,6 +140,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
             </TableFooter>
           </Table>
         </div>
+      ) : (
+        <OrderDetailsSkeleton />
       )}
     </DialogContent>
   )
